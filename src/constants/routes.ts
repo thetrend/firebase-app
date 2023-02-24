891ba337ed430166978b776397a26d89499c742c
Grace de la Mora <thetrend@users.noreply.github.com>
2023-02-24 00:31:00 -0500

    Adding React Router

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const ACCOUNT: string = '/account';
+export const ADMIN: string = '/admin';
+export const FORGOTPW: string = '/forgotpw';
+export const HOME: string = '/home';
+export const LANDING: string = '/';
+export const SIGNIN: string = '/signin';
+export const SIGNUP: string = '/signup';
